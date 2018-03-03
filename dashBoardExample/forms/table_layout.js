abdde2005ebe84a9c9f6087869577b18eb2fb28b
tuanway <tuanway@gmail.com>
2018-03-03 03:39:05 -0600

    updates

--- dashBoardExample/forms/table_layout.js
+++ dashBoardExample/forms/table_layout.js
@@ -9,8 +9,9 @@
  * @properties={typeid:24,uuid:"EFC2D52C-9D0D-4C21-BA2E-AB7C40D0CB05"}
  */
 function onAction$Header(event) {
-	var p = scopes.UI.getInstance(scopes.svyNavigation.getCurrentItem().getFormName())
-	var c = scopes.UI.getChildInstance(scopes.svyNavigation.getCurrentItem().getFormName(), controller.getName())
-	var i = new scopes.UI.Instance(p.getTitle() + ' - ' + c.getTitle(), controller.getName())
-	i.show();
+//	var p = scopes.UI.getInstance(scopes.svyNavigation.getCurrentItem().getFormName())	
+//	var c = scopes.UI.getChildInstance(scopes.svyNavigation.getCurrentItem().getFormName(), controller.getName())
+//	if (!c) return;
+//	var i = new scopes.UI.Instance(p.getTitle() + ' - ' + c.getTitle(), controller.getName())
+//	i.show();
 }
